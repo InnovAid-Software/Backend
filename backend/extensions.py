8e41d26f06e6fed36213b1559796dbb7adeb41f9
Christian Johnston <crjohnston@ualr.edu>
2024-10-22 19:19:14 -0500

    added email methods and extensions.  Go over this with abbie.

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,7 +1,10 @@
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
+message = Message()
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
 from backend.models.user import User, UserType
 from backend.models.registrationqueue import RegistrationQueue
-from backend.extensions import db
+from backend.extensions import db, mail, message
 from flask_bcrypt import Bcrypt
+from itsdangerous import URLSafeTimedSerializer
+
 
 bp = Blueprint('user', __name__)
 bcrypt = Bcrypt()
@@ -45,5 +47,21 @@ def verify(token):
 
 
 def send_verification_email(email, token):
-    # This is a dummy function for sending verification email
+    verification_url = f'http://innovaid.dev/verify/{token}'
+    msg = message('Please verify your email', recipients=[email])
+    msg.body = f'Click the link to verify your email: {verification_url}'
+    mail.send(msg) #see about declarations
     print(f"Sending verification email to {email} with token: {token}")
+
+#go over this with abbie later and see if these will work for token generation
+def generate_verification_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])  #work this out
+    return serializer.dumps(email, salt='email-confirmation-salt')
+
+def confirm_verification_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(token, salt='email-confirmation-salt', max_age=expiration)
+    except:
+        return False
+    return email
